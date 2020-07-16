22178723d673d61384274b31eb77d8f8ab6d35fe
Copter1998 <panutat10@gmail.com>
2020-07-16 10:09:24 +0700

    first commit

---
+++ src/components/Signin.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Signin extends Component {
+    render() {
+        return (
+            <div>
+                ถ้าโลกมันกลม ทำไมเธอไม่วนกลับมา !!
+            </div>
+        )
+    }
+}
---
+++ src/components/Signup.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Signup extends Component {
+    render() {
+        return (
+            <div>
+                tell me where do i fine , someone like you grl
+            </div>
+        )
+    }
+}
+
